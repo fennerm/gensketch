dd78d5b18e166560831950969bfb32ac0c5be2c0
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2023-05-01 11:26:02 -0700

    Partial jest setup for testing pixi

---
+++ src/lib/testUtil/fixtures.ts
@@ -0,0 +1,40 @@
+import type { UserConfig } from "@lib/bindings";
+
+export const fakeUserConfig = (): UserConfig => {
+  return {
+    styles: {
+      colors: {
+        background: 0xffffff,
+        alignment: 0x000000,
+        text: 0x000000,
+        error: 0xff0000,
+        errorBackground: 0xff0000,
+        trackLabelBackground: 0x000000,
+        secondaryText: 0xffffff,
+        nucleotideColors: {
+          A: 0xff0000,
+          G: 0x00ff00,
+          C: 0x0000ff,
+          T: 0x00000f,
+          N: 0x000000,
+          R: 0x000000,
+          Y: 0x000000,
+          K: 0x000000,
+          M: 0x000000,
+          S: 0x000000,
+          W: 0x000000,
+          B: 0x000000,
+          D: 0x000000,
+          H: 0x000000,
+          V: 0x000000,
+          GAP: 0x000000,
+        },
+        deletion: 0x000000,
+        insertion: 0x000000,
+      },
+      fonts: {
+        tooltipFontSize: 12,
+      },
+    },
+  };
+};
